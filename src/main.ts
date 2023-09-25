729e6b5826f7cc6f9ebd5ac18ecf6105e89079f2
khamlar <khamlarchanthavong@yahoo.com>
2023-09-25 21:30:34 +0700

    upload

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Mega branch')
+    .setDescription('The Mega branch API description')
+    .setVersion('1.0')
+    .addTag('Mega branch')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
