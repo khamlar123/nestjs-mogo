2a3917dbf8fe0b14fe738e75b6ce94f71c5d47a2
khamlar chanthavong <khamlar@khamlars-MacBook-Pro.local>
2023-12-11 21:53:23 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,19 @@
-
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { MenueModule } from './menu/menu.module'
+import { MenueModule } from './menu/menu.module';
 import { ConentsModule } from './conents/conents.module';
 
 @Module({
   imports: [
-     MongooseModule.forRoot('mongodb://localhost:27017/demo'), UsersModule, MenueModule, ConentsModule
-    ],
-  controllers: [
-     AppController],
+    MongooseModule.forRoot('mongodb://localhost:27017/mogo'),
+    UsersModule,
+    MenueModule,
+    ConentsModule,
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
